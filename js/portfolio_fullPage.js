59093624d6f7fc31d7dc7e1aff616a9262b516e0
Mario Martinez <zenitram.oiram@gmail.com>
2015-04-08 17:05:26 +0100

    images added

--- js/portfolio_fullPage.js
+++ js/portfolio_fullPage.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
   $('#fullpage').fullpage({
-      sectionsColor: ['#FFE30E', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff']
+      sectionsColor: ['#FFE30E', '#ACE3BF', '#7BAABE', 'whitesmoke', '#ccddff']
   });
 });
